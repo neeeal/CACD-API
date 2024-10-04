8988eb2fda15e0c19dfaaca0093be81d4c8ee805
neeeal <matira.nealbartonjames@gmail.com>
2024-10-04 18:26:55 +0800

    fix: fixed database connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
     mongoose
-    .connect(process.env.ATLAS_URI)
+    .connect(process.env.MONGODB_URI, {
+        dbName: process.env.MONGODB_DBNAME,
+    })
     .then(() => console.log("Database connected"))
     .catch((err) => console.error(err));
 };
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const Connection=require("./config/db.js");
+Connection();
 const cors = require('cors');
 const initRoutes = require("./routes");
 
 const app = express();
-Connection();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
@@ -25,39 +25,39 @@ initRoutes(app);
 
 // Middleware assign default status of not status is set
 app.use((req, res, next) => {
-    const originalSend = res.send;
+  const originalSend = res.send;
 
-    res.send = function (body) {
-        // Default to 200 if no status is set
-        if (!res.statusCode || res.statusCode === 200) {
-            res.status(200);
-        }
-        return originalSend.call(this, body);
-    };
+  res.send = function (body) {
+    // Default to 200 if no status is set
+    if (!res.statusCode || res.statusCode === 200) {
+      res.status(200);
+    }
+    return originalSend.call(this, body);
+  };
 
-    next();
+  next();
 });
 
 // Example route
 app.get('/api', (req, res) => {
-    try{
-        throw new Error('not implemented');
-    }
-    catch (err){
-        console.log("handled error")
-    }
-    res.send({ message: "handled error" });
+  try{
+    throw new Error('not implemented');
+  }
+  catch (err){
+    console.log("handled error")
+  }
+  res.send({ message: "handled error" });
 });
 
 // Error handling middleware
 // Default response code 500 if unhandled error
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    if (!res.headersSent) {
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  console.error(err.stack);
+  if (!res.headersSent) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.info(`Server is running on PORT: ${PORT}`);
+  console.info(`Server is running on PORT: ${PORT}`);
 });
