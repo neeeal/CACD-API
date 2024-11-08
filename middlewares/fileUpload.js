465202cc078b9686e7cccc0a5e7c11f51890eb71
neeeal <matira.nealbartonjames@gmail.com>
2024-11-08 21:52:36 +0800

    feat: add max file size

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -18,11 +18,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: function (req, file, cb) {
         const fileTypes = /jpeg|jpg|png|gif|jfif/;
         const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
         const mimetype = fileTypes.test(file.mimetype);
-
         if (extname && mimetype) {
             return cb(null, true);
         } else {
